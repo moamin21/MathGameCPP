f96f78ee9d92895e87f91fd2edfce9651bce6106
Mohamed Amin <moamin77q@gmail.com>
2025-01-29 07:38:58 +0200

    committed

--- main.cpp
+++ main.cpp
@@ -4,92 +4,92 @@
 
 using namespace std;
 
-enum enQuestionsLevel { Easy = 1, Medium = 2, Hard = 3 };
+enum en_questions_level { easy = 1, medium = 2, hard = 3 };
 
-int RandomNumber(int From, int To) {
+int rand_num(int From, int To) {
     return rand() % (To - From + 1) + From;
 }
 
-int GenerateQuestion(enQuestionsLevel level) {
-    int num1, num2, correctAnswer;
+int gen_question(en_questions_level level) {
+    int num1, num2, corr_ans;
     char operation;
 
     switch (level) {
-        case Easy:
-            num1 = RandomNumber(1, 10);
-            num2 = RandomNumber(1, 10);
+        case easy:
+            num1 = rand_num(1, 10);
+            num2 = rand_num(1, 10);
             break;
-        case Medium:
-            num1 = RandomNumber(10, 50);
-            num2 = RandomNumber(10, 50);
+        case medium:
+            num1 = rand_num(10, 50);
+            num2 = rand_num(10, 50);
             break;
-        case Hard:
-            num1 = RandomNumber(50, 100);
-            num2 = RandomNumber(50, 100);
+        case hard:
+            num1 = rand_num(50, 100);
+            num2 = rand_num(50, 100);
             break;
     }
 
-    int operationChoice = RandomNumber(1, 4);
-    switch (operationChoice) {
+    int operation_choice = rand_num(1, 4);
+    switch (operation_choice) {
         case 1:
             operation = '+';
-            correctAnswer = num1 + num2;
+            corr_ans = num1 + num2;
             break;
         case 2:
             operation = '-';
-            correctAnswer = num1 - num2;
+            corr_ans = num1 - num2;
             break;
         case 3:
             operation = '*';
-            correctAnswer = num1 * num2;
+            corr_ans = num1 * num2;
             break;
         case 4:
             operation = '/';
             num2 = (num2 == 0) ? 1 : num2;
-            correctAnswer = num1 / num2;
+            corr_ans = num1 / num2;
             break;
     }
 
     cout << "What is " << num1 << " " << operation << " " << num2 << "? ";
-    return correctAnswer;
+    return corr_ans;
 }
 
-void PlayMathGame() {
+void play_math_game() {
 
     int score = 0;
-    int numQuestions;
-    enQuestionsLevel level;
-    int levelChoice;
+    int num_questions;
+    en_questions_level level;
+    int level_choice;
 
     cout << "Welcome to the Math Quiz Game!\n";
 
     cout << "Choose difficulty level:\n1. Easy\n2. Medium\n3. Hard\n";
-    cin >> levelChoice;
+    cin >> level_choice;
 
-    level = static_cast<enQuestionsLevel>(levelChoice);
+    level = static_cast<en_questions_level>(level_choice);
 
     cout << "How many questions would you like to answer? ";
-    cin >> numQuestions;
+    cin >> num_questions;
 
-    for (int i = 1; i <= numQuestions; i++) {
+    for (int i = 1; i <= num_questions; i++) {
 
-        cout << "\nQuestion " << i << "/" << numQuestions << ":\n";
-        int correctAnswer = GenerateQuestion(level);
-        int playerAnswer;
+        cout << "\nQuestion " << i << "/" << num_questions << ":\n";
+        int corr_ans = gen_question(level);
 
-        cin >> playerAnswer;
+        int player_ans;
+        cin >> player_ans;
 
-        if (playerAnswer == correctAnswer) {
+        if (player_ans == corr_ans) {
             cout << "Correct! Great job!\n";
             score++;
         } else {
-            cout << "Incorrect. The correct answer was " << correctAnswer << ".\n";
+            cout << "Incorrect. The correct answer was " << corr_ans << ".\n";
         }
     }
 
-    cout << "\nYou answered " << score << " out of " << numQuestions << " questions correctly.\n";
+    cout << "\nYou answered " << score << " out of " << num_questions << " questions correctly.\n";
 
-    if (score >= numQuestions / 2) {
+    if (score >= num_questions / 2) {
         cout << "Congratulations! You passed the quiz!\n";
     } else {
         cout << "Better luck next time!\n";
@@ -99,13 +99,13 @@ void PlayMathGame() {
 int main() {
 
     srand(static_cast<unsigned>(time(0)));
-    char playAgain;
+    char play_again;
 
     do {
-        PlayMathGame();
+        play_math_game();
         cout << "\nWould you like to play again? (y/n): ";
-        cin >> playAgain;
-    } while (playAgain == 'Y' || playAgain == 'y');
+        cin >> play_again;
+    } while (play_again == 'Y' || play_again == 'y');
 
     cout << "Thank you for playing the Math Quiz Game! Goodbye!\n";
     return 0;
